e064a5c5cb8a09af52556c84fcadc5d9e7837ae2
knightza123 <knightza123@hotmail.com>
2018-03-31 18:11:05 +0700

    test body-parser

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
+const http = require('http');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const user = require('./routes/users.js');
 const PORT = 5000;
 user(app);
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.listen(PORT,()=>{
 	console.log('server listen on port : ',PORT);
 });
